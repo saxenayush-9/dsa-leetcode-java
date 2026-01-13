02edb65275592bc581a8c844962b90cfa777bc32
Ayush Saxena <43987160+saxenayush-9@users.noreply.github.com>
2026-01-13 13:22:15 +0530

    Time: 190 ms (5.47%) | Memory: 47.4 MB (6.05%) - LeetSync

--- 684-redundant-connection/redundant-connection.java
+++ 684-redundant-connection/redundant-connection.java
@@ -3,11 +3,15 @@ class Solution {
         if(parent[n]==n)return parent[n];
         return parent[n]=find(parent[n],parent);
     }
-    public void union(int a, int b, int[] parent){
+    public void union(int a, int b, int[] parent,int[] rank){
         int parentA = find(a,parent);
         int parentB = find(b,parent);
         if(parentA==parentB)return;
-        else if(parentA<parentB){
+        if(rank[parentA]==rank[parentB]){
+            parent[parentB]=parentA;
+            rank[parentA]++;
+        }
+        else if(rank[parentA]>rank[parentB]){
             parent[parentB]=parentA;
         }
         else{
@@ -17,8 +21,10 @@ class Solution {
     public int[] findRedundantConnection(int[][] edges) {
         int n = edges.length;
         int[] parent = new int[n+1];
+        int[] rank = new int[n+1];
         for(int i=0;i<=n;i++){
             parent[i]=i;
+            rank[i]=1;
         }
         for(int[] edge: edges){
             int a = edge[0];
@@ -27,7 +33,7 @@ class Solution {
             if(find(a,parent)==find(b,parent)){
                 return edge;
             }
-            union(a,b,parent);
+            union(a,b,parent,rank);
         }
         return new int[]{};
     }
