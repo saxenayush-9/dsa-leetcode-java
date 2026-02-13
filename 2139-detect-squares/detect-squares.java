dfa24819e4396985ca19749f48df9f879f864c73
Ayush Saxena <43987160+saxenayush-9@users.noreply.github.com>
2026-02-14 02:23:52 +0530

    Time: 587 ms (5.06%) | Memory: 61.3 MB (13.83%) - LeetSync

---
+++ 2139-detect-squares/detect-squares.java
@@ -0,0 +1,47 @@
+class DetectSquares {
+    List<String> points;
+    HashMap<String,Integer> frequency;
+
+    public DetectSquares() {
+        points = new ArrayList<>();
+        frequency = new HashMap<>();
+    }
+    
+    public void add(int[] point) {
+        String newPoint = point[0]+","+point[1];
+        points.add(newPoint);
+        if(frequency.containsKey(newPoint)){
+            frequency.put(newPoint,frequency.get(newPoint)+1);
+        }
+        else{
+            frequency.put(newPoint,1);
+        }
+    }
+    
+    public int count(int[] point) {
+        int x = point[0];
+        int y = point[1];
+        int totalNumberOfWays=0;
+        for(String pnt : points){
+            String[] currPoint = pnt.trim().split(",");
+            int px = Integer.parseInt(currPoint[0]);
+            int py = Integer.parseInt(currPoint[1]);
+            if(Math.abs(x-px)!=Math.abs(y-py) || px==x || py==y){
+                continue;
+            }
+            String topLeft = px+","+y;
+            String bottomRight = x+","+py;
+            if(frequency.containsKey(topLeft) && frequency.containsKey(bottomRight)){
+                totalNumberOfWays+=frequency.get(topLeft)*frequency.get(bottomRight);
+            }
+        }
+        return totalNumberOfWays;
+    }
+}
+
+/**
+ * Your DetectSquares object will be instantiated and called as such:
+ * DetectSquares obj = new DetectSquares();
+ * obj.add(point);
+ * int param_2 = obj.count(point);
+ */
