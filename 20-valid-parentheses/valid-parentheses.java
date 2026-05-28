1523004cb6bf1c1aa4b0424d11cea7e5b8947df3
Ayush Saxena <43987160+saxenayush-9@users.noreply.github.com>
2026-05-28 06:00:46 +0530

    Time: 3 ms (88.09%) | Memory: 43.6 MB (15.32%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,21 +1,22 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stk =new Stack<>();
+        Stack<Character> stk = new Stack<>();
         for(int i=0;i<s.length();i++){
             char ch = s.charAt(i);
-            if("({[".contains(ch+"")){
+            if(ch=='{' || ch=='[' || ch=='('){
                 stk.push(ch);
             }
             else{
-                if(stk.isEmpty())return false;
-                char top = stk.peek();
-                if(top=='(' && ch ==')'){
+                if(stk.isEmpty()){
+                    return false;
+                }
+                else if(ch=='}' && stk.peek()=='{'){
                     stk.pop();
                 }
-                else if(top=='{' && ch == '}'){
+                else if(ch==']' && stk.peek()=='['){
                     stk.pop();
                 }
-                else if(top=='[' && ch == ']'){
+                else if(ch==')' && stk.peek()=='('){
                     stk.pop();
                 }
                 else{
@@ -23,7 +24,6 @@ class Solution {
                 }
             }
         }
-        if(stk.isEmpty())return true;
-        return false;
+        return stk.isEmpty();
     }
 }
