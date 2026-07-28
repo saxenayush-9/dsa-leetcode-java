593c40811a2d4062ea3731df7aaa67f2e3aa9cf6
Ayush Saxena <43987160+saxenayush-9@users.noreply.github.com>
2026-07-28 21:23:00 +0530

    Time: 12 ms (51.40%) | Memory: 48.3 MB (16.58%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,40 +1,39 @@
 class Solution {
-    public long timeToEat(int[] piles,int n, int h){
-        long totalTime = 0;
-        for(int i=0;i<piles.length;i++){
-            if(piles[i]<=n){
-                totalTime++;
+    public boolean canEat(int[] arr, int num, int h){
+        int count = 0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]<=num){
+                count++;
             }
             else{
-                if(piles[i]%n==0){
-                    totalTime+=piles[i]/n;
-                }
-                else{
-                    totalTime+=piles[i]/n;
-                    totalTime++;
+                count+=arr[i]/num;
+                if(arr[i]%num!=0){
+                    count++;
                 }
             }
+            if(count>h)return false;
         }
-        return totalTime;
+        return true;
     }
-
     public int minEatingSpeed(int[] piles, int h) {
         int n = piles.length;
+        int minEatingSpeed = 1;
+        if(h<n)return -1;
         int max = Integer.MIN_VALUE;
-        for (Integer i : piles) {
-            max = Math.max(max, i);
+        for(Integer i : piles){
+            max = Math.max(max,i);
         }
         int low = 1, high = max;
-        int num = piles.length;
-        while (low <= high) {
-            int mid = low + ((high - low) / 2);
-            if (timeToEat(piles, mid, h) > h) {
-                low = mid + 1;
-            } else {
-                num = mid;
-                high = mid - 1;
+        while(low<=high){
+            int mid = low+((high-low)/2);
+            if(canEat(piles,mid,h)){
+                minEatingSpeed = mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
             }
         }
-        return num;
+        return minEatingSpeed;
     }
 }
