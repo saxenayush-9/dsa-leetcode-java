d7af959c586f912ad79e313d0a14d91cdd060ef4
Ayush Saxena <43987160+saxenayush-9@users.noreply.github.com>
2026-07-30 21:54:46 +0530

    Time: 3 ms (34.21%) | Memory: 45.8 MB (51.58%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,26 +1,22 @@
 class Solution {
     List<List<Integer>> list;
-    int[] nums;
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> list = new ArrayList<>();
         this.list=list;
-        this.nums=candidates;
-        List<Integer> li = new ArrayList<>();
-        solve(li,0,target);
+        findAllCombination(candidates,0,target,new ArrayList<>());
         return list;
     }
-    public void solve(List<Integer> li, int start,int target){
-        if(start==nums.length){
+    public void findAllCombination(int[] candidates, int start, int target, List<Integer> li){
+        if(target<0)return;
+        if(start==candidates.length){
             if(target==0){
                 list.add(new ArrayList<>(li));
             }
             return;
         }
-        if(target<0)return;
-        li.add(nums[start]);
-        solve(li,start,target-nums[start]);
+        li.add(candidates[start]);
+        findAllCombination(candidates, start, target-candidates[start],li);
         li.remove(li.size()-1);
-        solve(li,start+1,target);
+        findAllCombination(candidates, start+1, target,li);
     }
-
 }
