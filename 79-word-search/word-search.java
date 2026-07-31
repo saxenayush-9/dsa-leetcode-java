f9a0f460a69e3f03f3355933fa415515f2e5737b
Ayush Saxena <43987160+saxenayush-9@users.noreply.github.com>
2026-07-31 22:34:07 +0530

    Time: 159 ms (26.57%) | Memory: 43.1 MB (28.76%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -3,43 +3,39 @@ class Solution {
         int n = board.length;
         int m = board[0].length;
         boolean[][] visited = new boolean[n][m];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (visited[i][j])
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(visited[i][j]){
                     continue;
-                if (solve(board, visited, i, j, n, m, word, 0))
+                }
+                if(dfs(board,i,j,n,m,visited,word,0)){
                     return true;
+                };
             }
         }
         return false;
     }
 
-    public boolean solve(char[][] board, boolean[][] visited, int row, int col, int n, int m, String word,
-            int index) {
-        if (index == word.length()) {
+    public boolean dfs(char[][] board, int row, int col, int n, int m, boolean[][] visited, String word, int index){
+        if(index==word.length())return true;
+        if(row<0 || col<0)return false;
+        if(row>=n || col>=m)return false;
+        if(visited[row][col])return false;
+        visited[row][col]=true;
+        if(board[row][col]!=word.charAt(index)){
+            visited[row][col]=false;
+            return false;
+        }
+        if(
+            dfs(board,row+1,col,n,m,visited,word,index+1)||
+            dfs(board,row-1,col,n,m,visited,word,index+1)||
+            dfs(board,row,col+1,n,m,visited,word,index+1)||
+            dfs(board,row,col-1,n,m,visited,word,index+1)
+        ){
             return true;
         }
-        if (row < 0 || col < 0)
-            return false;
-        if (row >= n || col >= m)
-            return false;
-        if (visited[row][col])
-            return false;
-        if (board[row][col] != word.charAt(index))
-            return false;
-
-        visited[row][col] = true;
-        index++;
-        if (solve(board, visited, row + 1, col, n, m, word, index) ||
-                solve(board, visited, row - 1, col, n, m, word, index) ||
-                solve(board, visited, row, col + 1, n, m, word, index) ||
-                solve(board, visited, row, col - 1, n, m, word, index)) {
-            visited[row][col] = false;
-            index--;
-            return true;
-        }
-        visited[row][col] = false;
-        index--;
+        visited[row][col]=false;
         return false;
     }
 }
