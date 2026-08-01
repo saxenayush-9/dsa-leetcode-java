b9e23c4832be750d89f43d4cf4989e7e535ca9e6
Ayush Saxena <43987160+saxenayush-9@users.noreply.github.com>
2026-08-01 22:59:55 +0530

    Time: 10 ms (15.58%) | Memory: 65.4 MB (41.24%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -1,36 +1,35 @@
 class Solution {
     List<List<String>> list;
-
-    public List<List<String>> partition(String s) {
-        List<List<String>> list = new ArrayList<>();
-        this.list = list;
-        List<String> li = new ArrayList<>();
-        solve(li, 0, s);
-        return list;
-    }
-
-    public boolean palindrome(String s, int start, int end) {
-        while (start <= end) {
-            if (s.charAt(start) != s.charAt(end)) {
+    public boolean isPalindrome(String str){
+        int low=0,high=str.length()-1;
+        while(low<=high){
+            if(str.charAt(low)!=str.charAt(high)){
                 return false;
             }
-            start++;
-            end--;
+            low++;
+            high--;
         }
         return true;
     }
+    public List<List<String>> partition(String s) {
+        List<List<String>> list = new ArrayList<>();
+        this.list = list;
+        traverse(s,0,new ArrayList<>());
+        return list;
+    }
 
-    public void solve(List<String> li, int start, String s) {
-        if (start == s.length()) {
+    public void traverse(String s, int start, List<String> li){
+        if(start==s.length()){
             list.add(new ArrayList<>(li));
             return;
         }
-        for (int i = start; i < s.length(); i++) {
-            if (!palindrome(s, start, i))
+        for(int i=start;i<s.length();i++){
+            if(!isPalindrome(s.substring(start,i+1))){
                 continue;
-            li.add(s.substring(start, i + 1));
-            solve(li, i + 1, s);
-            li.remove(li.size() - 1);
+            }
+            li.add(s.substring(start,i+1));
+            traverse(s,i+1,li);
+            li.remove(li.size()-1);
         }
     }
 }
