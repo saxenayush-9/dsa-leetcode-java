05eb3ba926b5b4ee110baebe1c431c8a854d45af
Ayush Saxena <43987160+saxenayush-9@users.noreply.github.com>
2026-08-03 22:49:05 +0530

    Time: 6 ms (14.15%) | Memory: 47 MB (6.43%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -1,51 +1,60 @@
 class Solution {
     List<List<String>> list;
-    HashSet<Integer> columns = new HashSet<>();
-    HashSet<Integer> dig1 = new HashSet<>();
-    HashSet<Integer> dig2 = new HashSet<>();
-
     public List<List<String>> solveNQueens(int n) {
         List<List<String>> list = new ArrayList<>();
-        this.list = list;
+        this.list=list;
         List<String> li = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
+        for(int i=0;i<n;i++){
             StringBuilder sb = new StringBuilder();
-            for (int j = 0; j < n; j++) {
+            for(int j=0;j<n;j++){
                 sb.append(".");
             }
             li.add(sb.toString());
         }
-        solve(0, n, li);
+        boolean[] visitedCol = new boolean[n];
+        HashSet<Integer> diagonal1 = new HashSet<>();
+        HashSet<Integer> diagonal2 = new HashSet<>();
+
+        dfs(n, 0, visitedCol, li, diagonal1, diagonal2);
         return list;
     }
 
-    public void solve(int row, int n, List<String> li) {
-        if (row == n) {
+    public void dfs(int n, int row, boolean[] visitedCol, List<String> li,  HashSet<Integer> diagonal1,HashSet<Integer> diagonal2){
+        if(row==n){
             list.add(new ArrayList<>(li));
             return;
         }
+        for(int col=0;col<n;col++){
+            if(visitedCol[col] || diagonal1.contains(row-col) || diagonal2.contains(row+col)){
+                continue;
+            }
 
-        for (int col = 0; col < n; col++) {
-            if (columns.contains(col))
-                continue;
-            if (dig1.contains(row - col))
-                continue;
-            if (dig2.contains(row + col))
-                continue;
-            columns.add(col);
-            dig1.add(row - col);
-            dig2.add(row + col);
-            StringBuilder s = new StringBuilder(li.get(row));
-            s.setCharAt(col, 'Q');
-            li.set(row, s.toString());
-            solve(row + 1, n, li);
+            char[] currRow = li.get(row).toCharArray();
+            currRow[col]='Q';
+            StringBuilder sb = new StringBuilder();
+            for(int i=0;i<n;i++){
+                sb.append(currRow[i]);
+            }
+            li.set(row,sb.toString());
 
-            s = new StringBuilder(li.get(row));
-            s.setCharAt(col, '.');
-            li.set(row, s.toString());
-            columns.remove(col);
-            dig1.remove(row - col);
-            dig2.remove(row + col);
+            diagonal1.add(row-col);
+            diagonal2.add(row+col);
+            visitedCol[col]=true;
+
+            dfs(n, row+1, visitedCol, li,  diagonal1, diagonal2);
+
+            currRow = li.get(row).toCharArray();
+            currRow[col]= '.';
+            sb = new StringBuilder();
+            for(int i=0;i<n;i++){
+                sb.append(currRow[i]);
+            }
+
+            diagonal1.remove(row-col);
+            diagonal2.remove(row+col);
+            visitedCol[col]=false;
+
+            li.set(row,sb.toString());
         }
     }
 }
