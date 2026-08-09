7c8b70b8edd75c258e9eca3832244c8e55c17ddf
Ayush Saxena <43987160+saxenayush-9@users.noreply.github.com>
2026-08-09 13:38:46 +0530

    Time: 228 ms (58.63%) | Memory: 42.8 MB (73.11%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.java
+++ 37-sudoku-solver/sudoku-solver.java
@@ -2,16 +2,19 @@ class Solution {
     public void solveSudoku(char[][] board) {
         solve(board);
     }
-    public boolean isValid(char[][] board, int row, int col, char k){
+    public boolean valid(char[][] board, int row, int col, char ch){
         for(int i=0;i<9;i++){
-            if(board[row][i]==k)return false;
-            if(board[i][col]==k)return false;
+            if(board[row][i]==ch)return false;
+            if(board[i][col]==ch)return false;
         }
-        int boxRow = 3 * (row / 3);
-        int boxCol = 3 * (col / 3);
-        for (int r = 0; r < 3; r++) {
-            for (int c = 0; c < 3; c++) {
-                if (board[boxRow + r][boxCol + c] == k) return false;
+
+        int startRow = 3*(row/3);
+        int startCol = 3*(col/3);
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                if(board[startRow+i][startCol+j]==ch){
+                    return false;
+                }
             }
         }
         return true;
@@ -19,13 +22,15 @@ class Solution {
     public boolean solve(char[][] board){
         for(int i=0;i<9;i++){
             for(int j=0;j<9;j++){
-                if(board[i][j]=='.'){
+                char ch = board[i][j];
+                if(ch=='.'){
                     for(char k='1';k<='9';k++){
-                        if(isValid(board,i,j,k)){
-                            board[i][j]=k;
-                            if(solve(board))return true;
-                            board[i][j]='.';
+                        if(!valid(board,i,j,k)){
+                            continue;
                         }
+                        board[i][j]=k;
+                        if(solve(board))return true;
+                        board[i][j]='.';
                     }
                     return false;
                 }
