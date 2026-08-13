2a776361d5ff4e2dcffb03bc00a4eaffcb22fb77
Ayush Saxena <43987160+saxenayush-9@users.noreply.github.com>
2026-08-13 22:10:29 +0530

    Time: 15 ms (82.95%) | Memory: 46.8 MB (35.38%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -2,12 +2,13 @@ class Solution {
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
         int m = amount;
-        
+
         int[][] dp = new int[n+1][m+1];
-        for(int i=1;i<=m;i++){
-            dp[0][i]=Integer.MAX_VALUE-1;
+
+        for(int j=1;j<=m;j++){
+            dp[0][j]=Integer.MAX_VALUE-1;
         }
-        
+
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
                 if(j>=coins[i-1]){
@@ -18,6 +19,6 @@ class Solution {
                 }
             }
         }
-        return dp[n][m]==Integer.MAX_VALUE-1?-1:dp[n][m];
+        return (dp[n][m]==Integer.MAX_VALUE-1)?-1:dp[n][m];
     }
 }
