9fe9e2c1321b861fe18ccb51d11bfcdd02e83c2e
Ayush Saxena <43987160+saxenayush-9@users.noreply.github.com>
2026-08-15 14:58:43 +0530

    Time: 16 ms (3.02%) | Memory: 44.2 MB (7.65%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -1,31 +1,26 @@
 class Solution {
     public int numDecodings(String s) {
         int n = s.length();
-        int[] dp = new int[n];
-        int a = s.charAt(0)-'0';
-        if(a==0)return 0;
-        if(n==1)return 1;
-        int b = s.charAt(1)-'0';
-        int num = (a*10)+b;
-        dp[0]= 1;
-        if(10<=num && num<=26){
-            dp[1]++;
+        int[] dp = new int[n+1];
+
+        dp[0]=1;
+        if(s.charAt(0)=='0')return 0;
+        dp[1]=1;
+
+        for(int i=2;i<=n;i++){  
+            if(s.charAt(i-1)!='0'){
+                dp[i]=dp[i-1]; // not pairing
+            } 
+            if(s.charAt(i-2)!='0'){ // check for valid pair
+                String str = s.charAt(i-2)+""+s.charAt(i-1);
+                int num = Integer.parseInt(str);
+                System.out.println(num);
+                if(num<=26){
+                    dp[i]+=dp[i-2];
+                }
+            } 
         }
-        if(b>0){
-            dp[1]++;
-        }
-        for(int i=2;i<n;i++){
-            a = s.charAt(i-1)-'0';
-            b = s.charAt(i)-'0';
-            num = (a*10)+b;
-            dp[i]=0;
-            if(10<=num && num<=26){
-                dp[i]+=dp[i-2];
-            }
-            if(b>0){
-                dp[i]+=dp[i-1];
-            }
-        }
-        return dp[n-1];
+        System.out.println(Arrays.toString(dp));
+        return dp[n];
     }
 }
