fee4aa1da4730e69fe9cb7d587fae379733bd61d
Ayush Saxena <43987160+saxenayush-9@users.noreply.github.com>
2026-08-17 22:18:50 +0530

    Time: 8 ms (5.33%) | Memory: 49.2 MB (17.35%) - LeetSync

---
+++ 547-number-of-provinces/number-of-provinces.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int findCircleNum(int[][] isConnected) {
+        HashMap<Integer,List<Integer>> graph = new HashMap<>();
+        int count = 0;
+        int n = isConnected.length;
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(isConnected[i][j]==1){
+                    graph.computeIfAbsent(i, k -> new ArrayList<>()).add(j);
+                    graph.computeIfAbsent(j, k -> new ArrayList<>()).add(i);
+                }
+            }
+        }
+
+        boolean[] visited = new boolean[n];
+        for(int i=0;i<n;i++){
+            if(visited[i])continue;
+            dfs(graph,i,n,visited);
+            count++;
+        }
+        return count;
+    }
+
+    public void dfs(HashMap<Integer,List<Integer>> graph, int node, int n,boolean[] visited){
+        if(node<0 || node>=n) return;
+        if(visited[node])return;
+        visited[node]=true;
+        if(!graph.containsKey(node))return;
+        List<Integer> li = graph.get(node);
+        
+        for(Integer nbr: li){
+            if(visited[nbr])continue;
+            dfs(graph,nbr,n,visited);
+        }
+    }
+}
