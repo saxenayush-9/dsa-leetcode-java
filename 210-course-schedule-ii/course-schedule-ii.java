1673bd52851a19307182896962553f2c378bdcda
Ayush Saxena <43987160+saxenayush-9@users.noreply.github.com>
2026-08-22 23:51:38 +0530

    Time: 6 ms (43.42%) | Memory: 47.2 MB (25.35%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,58 +1,66 @@
 class Solution {
-    public int[] findOrder(int numCourses, int[][] prerequisites) {
-        int n = numCourses;
-        boolean[] visiting = new boolean[n];
-        boolean[] visited = new boolean[n];
-        HashMap<Integer, List<Integer>> graph = new HashMap<>();
-        for (int[] arr : prerequisites) {
-            int a = arr[0];
-            int b = arr[1];
-            if (!graph.containsKey(a)) {
-                List<Integer> li = new ArrayList<>();
-                li.add(b);
-                graph.put(a, li);
-            } else {
-                graph.get(a).add(b);
-            }
-        }
-        List<Integer> li = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            if (visited[i])
-                continue;
-            if (isCycle(graph, i, visited, visiting, li)) {
-                return new int[] {};
-            }
-        }
-        int[] res = new int[li.size()];
-        for (int i = 0; i < res.length; i++) {
-            res[i] = li.get(i);
-        }
-        return res;
-    }
 
-    public boolean isCycle(HashMap<Integer, List<Integer>> graph, int node, boolean[] visited, boolean[] visiting,
-            List<Integer> list) {
-        if (visiting[node])
-            return true;
-        if (visited[node])
-            return false;
-        if (!graph.containsKey(node)) {
-            visited[node] = true;
+    public boolean hasCycle( HashMap<Integer,List<Integer>> graph, int node, int[] state, List<Integer> list){
+        if(state[node]==1)return true;
+        if(state[node]==2)return false;
+        state[node]=1;
+        if(!graph.containsKey(node)){
+            state[node]=2;
             list.add(node);
             return false;
         }
-        visiting[node] = true;
         List<Integer> li = graph.get(node);
-        for (int i = 0; i < li.size(); i++) {
-            if (visited[li.get(i)])
-                continue;
-            if (isCycle(graph, li.get(i), visited, visiting, list)) {
+        for(Integer nbr : li){
+            if(state[nbr]==2)continue;
+            if(hasCycle(graph,nbr,state,list)){
                 return true;
             }
         }
-        visiting[node] = false;
-        visited[node] = true;
+        state[node]=2;
         list.add(node);
         return false;
+    }
+
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        int[] res = new int[numCourses];
+        HashMap<Integer,List<Integer>> graph = new HashMap<>();
+
+        if(prerequisites.length==0){
+            for(int i=0;i<numCourses;i++){
+                res[i]=i;
+            }
+            return res;
+        }
+
+        for(int[] course: prerequisites){
+            int from = course[0];
+            int to = course[1];
+
+            graph.computeIfAbsent(to,k -> new ArrayList<>()).add(from);
+
+        }
+
+        int[] state = new int[numCourses];
+        
+
+        List<Integer> list = new ArrayList<>();
+
+        for(int i=0;i<numCourses;i++){
+            if(state[i]==0 && hasCycle(graph,i,state,list)){
+                return new int[]{};
+            }
+        }
+
+        Collections.reverse(list);
+
+        for (int i = 0; i < numCourses; i++) {
+            if (state[i] == 0) list.add(i);
+        }
+
+        for (int i = 0; i < numCourses; i++) {
+            res[i] = list.get(i);
+        }
+
+        return res;
     }
 }
