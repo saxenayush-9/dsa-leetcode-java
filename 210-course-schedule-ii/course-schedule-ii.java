c22f44fe3b9eb7325cfb3604f423d627d24e4a89
Ayush Saxena <43987160+saxenayush-9@users.noreply.github.com>
2026-08-23 11:50:44 +0530

    Time: 20 ms (5.06%) | Memory: 47 MB (29.64%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,66 +1,68 @@
 class Solution {
 
-    public boolean hasCycle( HashMap<Integer,List<Integer>> graph, int node, int[] state, List<Integer> list){
+    public boolean hasCycle(HashMap<Integer,List<Integer>> graph, int node, int[] state, List<Integer> list){
         if(state[node]==1)return true;
         if(state[node]==2)return false;
+
         state[node]=1;
+
         if(!graph.containsKey(node)){
             state[node]=2;
             list.add(node);
             return false;
         }
+
         List<Integer> li = graph.get(node);
-        for(Integer nbr : li){
+
+        for(Integer nbr: li){
             if(state[nbr]==2)continue;
-            if(hasCycle(graph,nbr,state,list)){
-                return true;
-            }
+            if(hasCycle(graph,nbr,state,list))return true;
         }
-        state[node]=2;
         list.add(node);
+        state[node]=2;
         return false;
     }
 
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        int[] res = new int[numCourses];
         HashMap<Integer,List<Integer>> graph = new HashMap<>();
+        for(int[] pre: prerequisites){
+            int from = pre[0];
+            int to = pre[1];
 
-        if(prerequisites.length==0){
+            graph.computeIfAbsent(to,k-> new ArrayList<>()).add(from);
+        }
+
+        int[] state = new int[numCourses];
+
+        List<Integer> list = new ArrayList<>();
+        int[] res = new int[numCourses];
+
+        for(int i=0;i<numCourses;i++){
+            if(state[i]==0 && hasCycle(graph,i,state,list)){
+                return new int[] {};
+            }
+        }
+
+        if(list.isEmpty()){
             for(int i=0;i<numCourses;i++){
                 res[i]=i;
             }
             return res;
         }
-
-        for(int[] course: prerequisites){
-            int from = course[0];
-            int to = course[1];
-
-            graph.computeIfAbsent(to,k -> new ArrayList<>()).add(from);
-
-        }
-
-        int[] state = new int[numCourses];
         
-
-        List<Integer> list = new ArrayList<>();
+        Collections.reverse(list);
 
         for(int i=0;i<numCourses;i++){
-            if(state[i]==0 && hasCycle(graph,i,state,list)){
-                return new int[]{};
+            if(!list.contains(i)){
+                list.add(i);
             }
         }
 
-        Collections.reverse(list);
-
-        for (int i = 0; i < numCourses; i++) {
-            if (state[i] == 0) list.add(i);
-        }
-
-        for (int i = 0; i < numCourses; i++) {
-            res[i] = list.get(i);
+        for(int i=0;i<numCourses;i++){
+            res[i]=list.get(i);
         }
 
         return res;
+
     }
 }
