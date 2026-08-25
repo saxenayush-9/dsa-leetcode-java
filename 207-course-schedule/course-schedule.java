49ef32fc9b3f27cc217e8e10b1ef83c35656a609
Ayush Saxena <43987160+saxenayush-9@users.noreply.github.com>
2026-08-25 13:59:09 +0530

    Time: 7 ms (56.37%) | Memory: 47.3 MB (33.20%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,37 +1,42 @@
 class Solution {
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
 
-        int n = prerequisites.length;
-
-        if(n==0)return true;
-        HashMap<Integer,List<Integer>> graph = new HashMap<>();
-
-        for(int[] course : prerequisites){
-            int from = course[0];
-            int to = course[1];
-            graph.computeIfAbsent(to,k-> new ArrayList<>()).add(from);
-        }
-        
-        int[] state = new int[numCourses]; // 0=unvisited, 1=visiting, 2=visited
-        
-        
-        for(int i=0;i<numCourses;i++){
-            if(state[i]==0 && hasCycle(graph,i,state))return false;
-        }
-        return true;
-    }
-
-    public boolean hasCycle(HashMap<Integer,List<Integer>> graph, int node, int[] state){
+    public boolean hasCycle(int node, HashMap<Integer,List<Integer>> graph, int[] state){
         if(state[node]==1)return true;
         if(state[node]==2)return false;
 
-        if(!graph.containsKey(node))return false;
         state[node]=1;
-        List<Integer> li = graph.get(node);
-        for(int nbr : li){
-            if(hasCycle(graph,nbr,state))return true;
+        if(!graph.containsKey(node)){
+            state[node]=2;
+            return false;
         }
+        List<Integer> li = graph.get(node);
+
+        for(Integer nbr: li){
+            if(state[nbr]==2)continue;
+            if(hasCycle(nbr,graph,state))return true;
+        }
+
         state[node]=2;
         return false;
+    }
+
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        HashMap<Integer,List<Integer>> graph = new HashMap<>();
+
+        for(int[] prerequisite:prerequisites){
+            int from = prerequisite[0];
+            int to = prerequisite[1];
+
+            graph.computeIfAbsent(to,k->new ArrayList<>()).add(from);
+        }
+
+        int[] state = new int[numCourses];
+
+        for(int i=0;i<numCourses;i++){
+            if(state[i]==0 && hasCycle(i,graph,state)){
+                return false;
+            }
+        }
+        return true;
     }
 }
