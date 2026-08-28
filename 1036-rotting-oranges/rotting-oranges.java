d334198314f1d392ec90e618dee2dc1e1fcc94ad
Ayush Saxena <43987160+saxenayush-9@users.noreply.github.com>
2026-08-28 22:09:30 +0530

    Time: 9 ms (3.26%) | Memory: 46.9 MB (5.73%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -2,47 +2,59 @@ class Solution {
     public int orangesRotting(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
+
+        Queue<String> que = new LinkedList<>();
+
         boolean[][] visited = new boolean[n][m];
-        Queue<int[]> que = new LinkedList<>();
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 2) {
-                    que.add(new int[] { i, j });
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==2){
+                    String str = i+","+j;
+                    visited[i][j]=true;
+                    que.add(str);
                 }
             }
         }
 
-        int[][] dirs = { { -1, 0 }, { 0, -1 }, { 1, 0 }, { 0, 1 } };
-        while (!que.isEmpty()) {
-            int[] arr = que.remove();
-            int row = arr[0];
-            int col = arr[1];
-            for (int[] dir : dirs) {
-                int currRow = row + dir[0];
-                int currCol = col + dir[1];
+        int[][] dirs = {{-1,0},{0,-1},{1,0},{0,1}};
 
-                if (currRow >= 0 && currRow < n && currCol >= 0 && currCol < m && grid[currRow][currCol] == 1
-                        && !visited[currRow][currCol]) {
-                    visited[currRow][currCol] = true;
-                    grid[currRow][currCol] = grid[row][col] + 1;
-                    que.add(new int[] { currRow, currCol });
+        while(!que.isEmpty()){
+            String str = que.remove();
+            String[] st = str.trim().split(",");
+
+            int row = Integer.parseInt(st[0]);
+            int col = Integer.parseInt(st[1]);
+
+            for(int[] dir: dirs){
+                int currRow = row+dir[0];
+                int currCol = col+dir[1];
+
+                if(currRow>=0 && currCol>=0 && currRow<n && currCol<m && !visited[currRow][currCol] && grid[currRow][currCol]==1){
+                    visited[currRow][currCol]=true;
+                    grid[currRow][currCol]=grid[row][col]+1;
+                    str = currRow+","+currCol;
+                    que.add(str);
                 }
             }
         }
 
-        int minTime = 0;
+        int res = Integer.MIN_VALUE;
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 0 || grid[i][j] == 2)
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==0){
                     continue;
-                else if (grid[i][j] == 1) {
+                }
+                else if(grid[i][j]==1){
                     return -1;
-                } else {
-                    minTime = Math.max(minTime, grid[i][j]);
+                }
+                else{
+                    res= Math.max(res,grid[i][j]);
                 }
             }
         }
-        return minTime == 0 ? 0 : minTime - 2;
+        
+        return res==Integer.MIN_VALUE?0:res-2;
     }
 }
