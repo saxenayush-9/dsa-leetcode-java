eb30f704e1e06a4df31ff56053ea029ecd8f0f61
Ayush Saxena <43987160+saxenayush-9@users.noreply.github.com>
2026-09-09 23:13:42 +0530

    Time: 10 ms (98.33%) | Memory: 47.6 MB (66.68%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,25 +1,28 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        HashMap<Integer,Integer> hmap = new HashMap<>();
-        for(Integer num : nums){
-            if(!hmap.containsKey(num)){
-                hmap.put(num,1);
-            }
-            else{
-                hmap.put(num,hmap.get(num)+1);
-            }
-        }
         PriorityQueue<int[]> minHeap = new PriorityQueue<>((a,b)->a[1]-b[1]);
-        for(Integer key : hmap.keySet()){
-            minHeap.add(new int[]{key,hmap.get(key)});
+
+        HashMap<Integer,Integer> freq = new HashMap<>();
+
+        for(Integer i : nums){
+            freq.compute(i,(key,val)-> val==null?1:val+1);
+        }
+
+        for(Integer i : freq.keySet()){
+            minHeap.add(new int[] {i,freq.get(i)});
             if(minHeap.size()>k){
                 minHeap.remove();
             }
         }
-        int[] arr = new int[k];
-        for(int i=0;i<k;i++){
-            arr[i]=minHeap.remove()[0];
+
+        int[] res = new int[minHeap.size()];
+        int index=0;
+        
+        for(int[] arr: minHeap){
+            res[index]=arr[0];
+            index++;
         }
-        return arr;
+
+        return res;
     }
 }
