492adfb72d9bcec248c25a3b2f20340a2888a463
Ayush Saxena <43987160+saxenayush-9@users.noreply.github.com>
2026-09-12 22:08:16 +0530

    Time: 8 ms (90.11%) | Memory: 48.9 MB (74.71%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,42 +1,47 @@
 class Solution {
-    public boolean overlap(int[] interval1, int[] interval2) {
-        int s1 = interval1[0];
-        int e1 = interval1[1];
-        int s2 = interval2[0];
-        int e2 = interval2[1];
-        if (e2 < s1 || e1 < s2)
-            return false;
-        return true;
-    }
-
-    public int[] merge(int[] interval1, int[] interval2) {
-        int s1 = interval1[0];
-        int e1 = interval1[1];
-        int s2 = interval2[0];
-        int e2 = interval2[1];
-        return new int[] { Math.min(s1, s2), Math.max(e1, e2) };
-    }
-
     public int[][] merge(int[][] intervals) {
-        if (intervals.length == 1)
-            return intervals;
-        int n = intervals.length;
-        List<int[]> list = new ArrayList<>();
-        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
 
-        for(int i=1;i<n;i++){
-            if(!overlap(intervals[i],intervals[i-1])){
+        Arrays.sort(intervals,(a,b)->a[0]-b[0]);
+
+        List<int[]> list = new ArrayList<>();
+
+        for(int i=1;i<intervals.length;i++){
+            if(!doesOverlap(intervals[i],intervals[i-1])){
                 list.add(intervals[i-1]);
             }
             else{
-                intervals[i]=merge(intervals[i],intervals[i-1]);
+                intervals[i]=merge(intervals[i-1],intervals[i]);
             }
         }
-        list.add(intervals[n-1]);
+
+        list.add(intervals[intervals.length-1]);
+
         int[][] arr = new int[list.size()][2];
-        for (int j = 0; j < arr.length; j++) {
-            arr[j] = list.get(j);
+
+        for(int i=0;i<list.size();i++){
+            arr[i]=list.get(i);
         }
+
         return arr;
+        
+    }
+
+    public int[] merge(int[] interval1, int[] interval2){
+        int s1 = interval1[0];
+        int e1 = interval1[1];
+        int s2 = interval2[0];
+        int e2 = interval2[1];
+
+        return new int[]{Math.min(s1,s2),Math.max(e1,e2)};
+    }
+
+    public boolean doesOverlap(int[] interval1, int[] interval2){
+        int s1 = interval1[0];
+        int e1 = interval1[1];
+        int s2 = interval2[0];
+        int e2 = interval2[1];
+
+        if(e1<s2 || e2<s1)return false;
+        return true;
     }
 }
